cd3c97c7b7ee50ad016fdb43e5a1cee8b17941ad
ingridyang <43019677+ingridyang@users.noreply.github.com>
2019-05-05 21:50:26 -0700

    Create lightshow.ino

---
+++ lightshow.ino
@@ -0,0 +1,46 @@
+int soundsensor = A0;
+int led1 = 7;
+int led2 = 10;
+int led3 = 12;
+//boolean LEDStatus=false;
+
+void setup() {
+
+  pinMode (soundsensor, INPUT);
+  pinMode (led1, OUTPUT);
+  pinMode (led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+int val = analogRead (soundsensor);
+  Serial.println(val);
+
+  if (val > 350)
+  {
+    digitalWrite (led1, HIGH);
+  }
+  else
+  {
+    digitalWrite (led1, LOW);
+  }
+  
+  
+ if (val > 370)
+  {
+    digitalWrite (led2, HIGH);
+  }
+  else
+  {
+    digitalWrite (led2, LOW);
+  }
+
+  
+  if (val > 390)
+  {
+    digitalWrite (led3, HIGH);
+  }
+  else
+  {
+    digitalWrite (led3, LOW);
+  }
+}
