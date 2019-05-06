ef9b2e24ec1ed91595c078b940abb656de0628eb
ingridyang <43019677+ingridyang@users.noreply.github.com>
2019-05-06 13:12:48 -0700

    Update lightshow.ino

--- lightshow.ino
+++ lightshow.ino
@@ -1,14 +1,17 @@
 int soundsensor = A0;
 int led1 = 7;
-int led2 = 10;
-int led3 = 12;
+int led2 = 8;
+int led3 = 10;
+int led4 = 12;
+int led5 = 13;
+
 
 void setup() {
-
   pinMode (soundsensor, INPUT);
   pinMode (led1, OUTPUT);
   pinMode (led2, OUTPUT);
   pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 }
 
 void loop() {
@@ -34,13 +37,34 @@ void loop() {
     digitalWrite (led2, LOW);
   }
 
-
-  if (val > 390)
+ 
+ if (val > 380)
   {
     digitalWrite (led3, HIGH);
   }
   else
   {
     digitalWrite (led3, LOW);
+  }
+
+  
+  
+  if (val > 390)
+  {
+    digitalWrite (led4, HIGH);
+  }
+  else
+  {
+    digitalWrite (led4, LOW);
+  }
+
+
+   if (val > 350)
+  {
+    digitalWrite (led5, HIGH);
+  }
+  else
+  {
+    digitalWrite (led5, LOW);
   }
 }
